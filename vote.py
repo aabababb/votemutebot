bc52366f7346b0d2d0a0b8cdf5d907093e5196f2
aabababb <471352702@qq.com>
2020-04-09 16:43:43 +0800

    v2

--- vote.py
+++ vote.py
@@ -1,10 +1,9 @@
-﻿# coding=UTF-8
+# coding=UTF-8
 import logging
 import time
 #from pyrogram import ChatPermissions
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup,ChatPermissions
-
 
 class KickUser(object):
     def __init__(self, message):
@@ -16,7 +15,11 @@ class KickUser(object):
         self.name = message.from_user.name
         self.agree = []
         self.disagree = []
-
+        self.mutetime2 = 0
+        self.chatmembers = 0
+        self.karg = ''
+        self.ktext = ''
+        
     def add_agree(self, user):
         if user.id in self.vote_user():
             return False
@@ -34,12 +37,33 @@ class KickUser(object):
 
     def disagree_counter(self):
         return len(self.disagree)
+    
+    def get_karg(self,arg):
+        self.karg = arg
+        print("self.karg=%s" % self.karg)
+        return (self.karg)
+    
+    def get_ktext(self,text):
+        self.ktext = text
+        print("self.ktext=%s" % self.ktext)
+        return (self.ktext)        
+    
+    def get_mutetime2(self,time):
+        print("time=%s" % time)
+        self.mutetime2 = time
+        return (self.mutetime2)
+
+    def get_chatmembers(self,members):
+        print("members=%s" % members)
+        self.chatmembers = members
+        return (self.chatmembers)
 
     def vote_counter(self):
         return self.agree_counter()+self.disagree_counter()
 
     def vote_user(self):
         return self.agree + self.disagree
+    
 
     def log(self):
         return "{},{},{},{}".format(self.user_id, self.name, self.agree_counter(), self.disagree_counter())
@@ -88,8 +112,12 @@ def delete_keyboard(key):
     return InlineKeyboardMarkup(delete)
 
 
-def fuck(bot, update):
-    bot.deleteWebhook()
+def fuck(bot, update,args):    
+    print("bot=%s" % bot)
+    #print("update=%s" % update)
+
+    mutetime1=60
+    #mutetime1=args[0]
     kick_message = update.message.reply_to_message
     if update.message.chat.type == 'private':
         text = '此命令在私聊中不可用'
@@ -101,7 +129,45 @@ def fuck(bot, update):
         return
 
     kick_user = KickUser(kick_message)
+    if len(args) == 0:
+        arg = 'h'
+        kick_user.get_karg(arg)
+    else:
+    	print("args0=%s" % args[0])
+    	arg = args[0]
+    	kick_user.get_karg(arg)
 
+    print("kick_user.karg=%s" % kick_user.karg)
+    if kick_user.karg == 'd':
+        mutetime2 = 86400
+        chatmembers = 50
+        text = "一天"
+        kick_user.get_ktext(text) 
+        kick_user.get_mutetime2(mutetime2)        
+        kick_user.get_chatmembers(chatmembers)
+    elif kick_user.karg == 'm':
+        mutetime2 = 2592000
+        chatmembers = 10
+        text = "一月"
+        kick_user.get_ktext(text) 
+        kick_user.get_mutetime2(mutetime2)        
+        kick_user.get_chatmembers(chatmembers)
+    elif  kick_user.karg == 'f':
+        mutetime2 = 6
+        chatmembers = 2
+        text = "永久"
+        kick_user.get_ktext(text) 
+        kick_user.get_mutetime2(mutetime2)        
+        kick_user.get_chatmembers(chatmembers)
+    else:
+        mutetime2=3600
+        chatmembers = 100
+        text = "一小时"
+        kick_user.get_ktext(text) 
+        kick_user.get_mutetime2(mutetime2)        
+        kick_user.get_chatmembers(chatmembers)
+    print("mutetime2=%s" % mutetime2)
+    
     if kick_user.user_id == bot.get_me().id:
     	text = '【{} {}】总有刁民想害朕!'.format(update.effective_user.name,kick_user.name)
     	bot.send_message(kick_user.chat_id,text)
@@ -114,7 +180,7 @@ def fuck(bot, update):
         bot.restrict_chat_member(
             kick_user.chat_id,
             kick_user.user_id,
-            until_date=int(time.time()+480),
+            until_date=int(time.time()+mutetime1),
             permissions=ChatPermissions(
                 can_send_messages=False,
                 can_send_media_messages=False,
@@ -127,7 +193,7 @@ def fuck(bot, update):
         )
     except BaseException as e:
         logging.error(e)
-        text = '【{} {}】刚才发生了什么？'.format(update.effective_user.name,kick_user.name)
+        text = '【{} {}】刚才发生了什么？'.format(bot.effective_user.name,kick_user.name)
         bot.send_message(kick_user.chat_id, text)
         bot.delete_message(kick_user.chat_id, update.message.message_id)
         return
@@ -138,7 +204,7 @@ def fuck(bot, update):
 ##        new_job = context.job_queue.run_once(alarm, due, context=chat_id)
 ##        context.chat_data['job'] = new_job     
 
-        base_text = '正在投票禁言群成员【{} {}】.\n产生投票结果前，该用户被禁言8分钟，请尽快投票！\n'.format(kick_user.name,kick_user.user_id)
+        base_text = '正在投票禁言群成员【{} {} 】  {}.\n产生投票结果前，该用户被禁言8分钟，请尽快投票！\n'.format(kick_user.name,kick_user.user_id,kick_user.ktext)
 
         kickusers.save_kickuser(key, kick_user)
 
@@ -154,25 +220,28 @@ def fuck(bot, update):
             reply_markup=menu_keyboard(key, kick_user.agree_counter(), kick_user.disagree_counter())
         )
     except (IndexError, ValueError):
-        update.message.reply_text('Usage: /set <seconds>')
+        update.message.reply_text('Usage: /fuck <seconds>')
 
     
    
 def vote(bot, update):
-
         query = update.callback_query
+        print("query=%s" % query)
         msg = query.message
         cmd = query.data.split(' ')[0]
         key = query.data.split(' ')[1]
 
         kick_user = kickusers.get_kickuser(key)
+        print("vmutetime2=%s" % kick_user.mutetime2)
+        print("vchatmembers=%s" % kick_user.chatmembers)
+       
         allmembers = bot.get_chat_members_count(kick_user.chat_id)
         try:
-            max_vote = int(allmembers/100)
+            max_vote = int(allmembers/kick_user.chatmembers)
             if max_vote <= 2:
                 max_vote = 3
             else:
-                max_vote = int(allmembers/100)
+                max_vote = int(allmembers/kick_user.chatmembers)
                 
         except Exception as e:
             logging.error(e)
@@ -207,7 +276,7 @@ def vote(bot, update):
         if kick_user.vote_counter() >= max_vote:
             return
 
-        base_text = '正在投票禁言群成员【{} {}】.\n产生投票结果前，该用户被禁言8分钟，请尽快投票！\n'.format(kick_user.name,kick_user.user_id)
+        base_text = '正在投票禁言群成员【{} {} 】  {}.\n产生投票结果前，该用户被禁言8分钟，请尽快投票！\n'.format(kick_user.name,kick_user.user_id,kick_user.ktext)
 
         if  kick_user.vote_counter() < max_vote:
         
@@ -246,7 +315,7 @@ def vote(bot, update):
 
             if kick_user.vote_counter() == max_vote:
                 if kick_user.agree_counter() > kick_user.disagree_counter():
-                    text = '经投票,同意禁言【{} {}】1小时'.format(kick_user.name,kick_user.user_id)
+                    text = '经投票,同意禁言【{} {} 】 {}'.format(kick_user.name,kick_user.user_id,kick_user.ktext)
                     #bot.kick_chat_member(kick_user.chat_id, kick_user.user_id)
                     bot.restrict_chat_member(
                         kick_user.chat_id,
@@ -260,7 +329,7 @@ def vote(bot, update):
                             can_change_info=False,
                             can_invite_users=False,
                             can_pin_messages=False),
-                        until_date=int(time.time()+3600)
+                        until_date=int(time.time()+kick_user.mutetime2)
                         )
                 else:
                     text = '经投票,不同意禁言【{} {}】,已恢复该用户所有权限'.format(kick_user.name,kick_user.user_id)
