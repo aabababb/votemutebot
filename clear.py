bf7a9888135be860a9c440e903c7251bbddb870f
aabababb <471352702@qq.com>
2020-04-19 06:27:01 +0800

    v2.2

--- clear.py
+++ clear.py
@@ -3,11 +3,13 @@ import re
 def clear_message(bot, update):
     bot.delete_message(chat_id=update.message.chat_id,message_id=update.message.message_id)
                        
-    name1 = update.message.from_user.name
-    num = re.match("\d{8}",name1)
+    fname = update.message.from_user.first_name
+    uname = update.message.from_user.username
+    fnum = re.match("\d{8}",fname)
+    unum = re.match("\d{8}",uname)
     
-    if len(name1) > 20 or  num != None:
+    if len(fname) > 30 or  fnum != None or unum != None:
             
         bot.kick_chat_member(update.message.chat_id, update.message.from_user.id,until_date=60)
-        text = '{} 名字有广告，被踢出！'.format(name1)
+        text = '{} 名字有广告，被踢出！'.format(fname)
         bot.send_message(update.message.chat_id, text)
