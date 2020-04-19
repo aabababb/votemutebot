ffad53a699f631a488b0c16c000a5384f3253092
aabababb <471352702@qq.com>
2020-04-19 16:25:49 +0800

    v2.3

--- clear.py
+++ clear.py
@@ -1,15 +1,18 @@
 import re
 
 def clear_message(bot, update):
-    bot.delete_message(chat_id=update.message.chat_id,message_id=update.message.message_id)
-                       
-    fname = update.message.from_user.first_name
-    uname = update.message.from_user.username
-    fnum = re.match("\d{8}",fname)
-    unum = re.match("\d{8}",uname)
-    
-    if len(fname) > 30 or  fnum != None or unum != None:
-            
-        bot.kick_chat_member(update.message.chat_id, update.message.from_user.id,until_date=60)
-        text = '{} 名字有广告，被踢出！'.format(fname)
-        bot.send_message(update.message.chat_id, text)
+    try:
+        bot.delete_message(chat_id=update.message.chat_id,message_id=update.message.message_id)
+                           
+        fname = update.message.from_user.first_name
+        uname = update.message.from_user.username
+        fnum = re.match("\d{8}",fname)
+        unum = re.match("\d{8}",uname)
+        
+        if len(fname) > 30 or  fnum != None or unum != None:
+                
+            bot.kick_chat_member(update.message.chat_id, update.message.from_user.id,until_date=60)
+            text = '{} 名字有广告，被踢出！'.format(fname)
+            bot.send_message(update.message.chat_id, text)
+    except Exception as e:
+        logging.error(e)
