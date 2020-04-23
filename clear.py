5783b8745bd178478a4324560000be6096ee0e78
aabababb <471352702@qq.com>
2020-04-24 00:49:06 +0800

    v2.3.8

--- clear.py
+++ clear.py
@@ -1,10 +1,12 @@
 import re
 import logging
 import time
+from telegram import ChatPermissions
 def clear_message(bot, update):
-    try:
-        bot.delete_message(chat_id=update.message.chat_id,message_id=update.message.message_id)
-               
+    try:        
+        ltext=len(update.message.text)
+        print("ftext=%s" % update.message)
+        print("ftext=%s" % ltext)     
         fname = update.message.from_user.first_name
         fnum = re.search("\d{8}",fname)
         strname = 0
@@ -30,5 +32,23 @@ def clear_message(bot, update):
             bot.kick_chat_member(update.message.chat_id, update.message.from_user.id,until_date=int(time.time()+60))
             text = '【{}】 名字有广告，被踢出！'.format(fname)
             bot.send_message(update.message.chat_id, text)
+        if ltext > 666:
+            bot.delete_message(chat_id=update.message.chat_id,message_id=update.message.message_id)
+            bot.restrict_chat_member(
+                update.message.chat_id,
+                update.message.from_user.id,
+                until_date=int(time.time()+3600),
+                permissions=ChatPermissions(
+                    can_send_messages=False,
+                    can_send_media_messages=False,
+                    can_send_polls=False,
+                    can_send_other_messages=False,
+                    can_add_web_page_previews=False,
+                    can_change_info=False,
+                    can_invite_users=False,
+                    can_pin_messages=False))
+            text = '【{}】 刷屏，禁言1小时'.format(fname)
+            bot.send_message(update.message.chat_id, text)
+
     except Exception as e:
         logging.error(e)
