5bcee4718d754781958fabca1f3dca83ebe8915b
aabababb <471352702@qq.com>
2020-04-24 13:34:32 +0800

    v2.3.10

--- clear.py
+++ clear.py
@@ -2,11 +2,18 @@ import re
 import logging
 import time
 from telegram import ChatPermissions
-def clear_message(bot, update):
+def clear_tmessage(bot, update):
     try:        
         ltext=len(update.message.text)
         print("ftext=%s" % update.message)
-        print("ltext=%s" % ltext)     
+        print("ltext=%s" % ltext)
+        tnum1 = re.search("\d{8}",update.message.text)
+        
+        qtext = re.findall('[\u4e00-\u9fa5a-zA-Z0-9]+',update.message.text,re.S)   #只要字符串中的中文，字母，数字
+        qtext = "".join(qtext)
+        print("qtext=%s" % qtext)
+        tnum2 = re.search("\d{8}",qtext)
+        
         fname = update.message.from_user.first_name
         fnum = re.search("\d{8}",fname)
         strname = 0
@@ -30,7 +37,7 @@ def clear_message(bot, update):
                 
         if len(fname) > 30 or strname > 20 or fnum != None or lnum != None or unum != None:
             bot.kick_chat_member(update.message.chat_id, update.message.from_user.id,until_date=int(time.time()+60))
-            text = '【{}】 名字有广告，被踢出！'.format(fname)
+            text = '【{},{}】 名字有广告，被踢出！'.format(fname,lname)
             bot.send_message(update.message.chat_id, text)
         if ltext > 666:
             bot.delete_message(chat_id=update.message.chat_id,message_id=update.message.message_id)
@@ -47,7 +54,72 @@ def clear_message(bot, update):
                     can_change_info=False,
                     can_invite_users=False,
                     can_pin_messages=False))
-            text = '【{}】 刷屏，禁言1小时'.format(fname)
+            text = '【{}】 刷屏，禁言1小时'.format(update.message.from_user.name)
+            bot.send_message(update.message.chat_id, text)
+        if tnum1 != None:
+            bot.delete_message(chat_id=update.message.chat_id,message_id=update.message.message_id)
+            bot.restrict_chat_member(
+            update.message.chat_id,
+            update.message.from_user.id,
+            until_date=int(time.time()+3600*8),
+            permissions=ChatPermissions(
+                can_send_messages=False,
+                can_send_media_messages=False,
+                can_send_polls=False,
+                can_send_other_messages=False,
+                can_add_web_page_previews=False,
+                can_change_info=False,
+                can_invite_users=False,
+                can_pin_messages=False))
+            text = '【{}】 乱发广告，禁言8小时'.format(update.message.from_user.name)
+            bot.send_message(update.message.chat_id, text)
+        if tnum2 != None:
+            bot.delete_message(chat_id=update.message.chat_id,message_id=update.message.message_id)
+            bot.restrict_chat_member(
+                update.message.chat_id,
+                update.message.from_user.id,
+                until_date=int(time.time()+60),
+                permissions=ChatPermissions(
+                    can_send_messages=False,
+                    can_send_media_messages=False,
+                    can_send_polls=False,
+                    can_send_other_messages=False,
+                    can_add_web_page_previews=False,
+                    can_change_info=False,
+                    can_invite_users=False,
+                    can_pin_messages=False))
+
+    except Exception as e:
+        logging.error(e)
+
+
+def clear_jlmessage(bot, update):
+    try: 
+        bot.delete_message(chat_id=update.message.chat_id,message_id=update.message.message_id)       
+        fname = update.message.from_user.first_name
+        fnum = re.search("\d{8}",fname)
+        strname = 0
+        lnum = None
+        unum = None
+        try:
+            lname = update.message.from_user.last_name
+            print("lname=%s" % lname)
+            lnum = re.search("\d{8}",lname)
+            print("lnum=%s" % lnum)
+            strname=len(lname)
+        except Exception as e:
+            logging.error(e)
+        try:
+            uname = update.message.from_user.username
+            print("uname=%s" % uname)
+            unum = re.search("\d{8}",uname)
+            print("unum=%s" % unum)
+        except Exception as e:
+            logging.error(e)
+                
+        if len(fname) > 30 or strname > 20 or fnum != None or lnum != None or unum != None:
+            bot.kick_chat_member(update.message.chat_id, update.message.from_user.id,until_date=int(time.time()+60))
+            text = '【{},{}】 名字有广告，被踢出！'.format(fname,lname)
             bot.send_message(update.message.chat_id, text)
 
     except Exception as e:
