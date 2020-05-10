26fd0b4e5cab3c7c0aaef831843cff4058301a46
aabababb <471352702@qq.com>
2020-05-10 17:23:46 +0800

    v2.1

--- findid.py
+++ findid.py
@@ -1,9 +1,19 @@
 # coding=UTF-8
+import logging
+
 def findid(bot, update,args):
-    chat_id = update.message.chat_id    
+    chat_id = update.message.chat_id
+    rmessage=update.message.reply_to_message
     if len(args) == 0:
-        text = '【{}】你的ID号码为【{}】'.format(update.message.from_user.name,update.message.from_user.id)
-        bot.send_message(chat_id, text,parse_mode='HTML',disable_web_page_preview=True)
+        try:
+            if rmessage:
+                text = '【{}】,查到【{}】的ID号码为【{}】'.format(update.effective_user.name,rmessage.from_user.name,rmessage.from_user.id)
+                bot.send_message(chat_id, text,parse_mode='HTML',disable_web_page_preview=True)
+            else:
+                text = '【{}】你的ID号码为【{}】'.format(update.message.from_user.name,update.message.from_user.id)
+                bot.send_message(chat_id, text,parse_mode='HTML',disable_web_page_preview=True)
+        except Exception as e:
+            logging.error(e)
     else:
         print("args0=%s" % args[0])
         arg = args[0]
