90395db9bea6b09bb6de62786d2f5e264941882d
aabababb <471352702@qq.com>
2022-05-26 20:03:20 +0800

    v2.6

--- filters.py
+++ filters.py
@@ -1,13 +1,11 @@
 from telegram.ext import Filters
 
-
 def new_and_left_filter():
     return Filters.status_update.new_chat_members | Filters.status_update.left_chat_member
-
 status_update = new_and_left_filter()
 
 def ftext():
-    return Filters.text
-
+    return Filters.update
+    #return Filters.text
 
 ftext = ftext()
